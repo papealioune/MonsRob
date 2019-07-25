f0ea7efdc25ddae3546808cae60aecd58586874b
Pape Alioune <papealioune@MacBook-Pro-de-me.local>
2019-07-25 18:37:40 +0000

    Updates

---
+++ precache-manifest.8697a01deb1383efd58213e4f6c4d98a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53e3bd6fa6177120be186bd003cf15a",
+    "url": "/MonsRob/index.html"
+  },
+  {
+    "revision": "7d6a02febe181a7ff7a1",
+    "url": "/MonsRob/static/css/main.03d9cc94.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/MonsRob/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7d6a02febe181a7ff7a1",
+    "url": "/MonsRob/static/js/main.03e53a8a.chunk.js"
+  },
+  {
+    "revision": "f40b78f609bf72987e6d",
+    "url": "/MonsRob/static/js/runtime~main.7e5f300a.js"
+  }
+]);
